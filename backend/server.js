2eaacb5ea693ad7e112e46e080ffa38c2fb3fe92
Adekola Muhammed <adekolamuhammed@Adekolas-MacBook-Pro.local>
2025-08-29 02:21:28 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/static', express.static(path.join(__dirname, '../frontend')));
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/home.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+    console.log(__dirname)
+});
